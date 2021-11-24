7f82bcb15350e9bffdff02094670de90ca2db33a
sdanieau8 <psdanieau@gmail.com>
2021-11-23 18:25:02 -0800

    updated photos for beaches page

--- src/components/directory/data/beaches.data.js
+++ src/components/directory/data/beaches.data.js
@@ -2,30 +2,30 @@ const BEACHES_DATA = [
   {
     id: 12,
     name: 'Pacific Beach',
-    imageUrl: 'https://i.ibb.co/MhGrm06/lajolla.jpg',
+    imageUrl: 'https://i.ibb.co/pWgqDMT/pacificbeach3.jpg',
     address: '123 Beach Way',
-    notes: '"Surfing, swimming, family friendly"'
+    notes: '"Surfing, swimming, "'
   },
   {
     id: 13,
     name: 'Mission Beach',
-    imageUrl: 'https://i.ibb.co/MhGrm06/lajolla.jpg',
+    imageUrl: 'https://i.ibb.co/pWgqDMT/pacificbeach3.jpg',
     address: '456 Beach Way',
-    notes: '"Surfing, swimming, family friendly"'
+    notes: '"Surfing, swimming, "'
   },
   {
     id: 14,
-    name: 'La Jolla',
-    imageUrl: 'https://i.ibb.co/MhGrm06/lajolla.jpg',
+    name: 'La Jolla Cove',
+    imageUrl: 'https://i.ibb.co/pWgqDMT/pacificbeach3.jpg',
     address: '789 Beach Way',
-    notes: '"Seals, snorkeling, kayaking"'
+    notes: '"Seals, snorkeling, cliffs, caves"'
   },
   {
     id: 15,
     name: 'Coronado',
-    imageUrl: 'https://i.ibb.co/MhGrm06/lajolla.jpg',
+    imageUrl: 'https://i.ibb.co/pWgqDMT/pacificbeach3.jpg',
     address: '101 Beach Way',
-    notes: '"Swimming, family friendly"'
+    notes: '"Swimming, "'
   }
 
 ];
