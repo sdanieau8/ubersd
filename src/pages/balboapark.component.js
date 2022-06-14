deef8a1f0d32c1f8c16389e8398e46f232edc0ed
sdanieau8 <psdanieau@gmail.com>
2022-06-14 14:40:11 -0700

    added google code

--- src/pages/balboapark.component.js
+++ src/pages/balboapark.component.js
@@ -3,6 +3,7 @@ import Sidebar from "../components/sidebar.component";
 import GoogleMaps from "../components/map.component";
 import './secondary.styles.scss';
 import Weather from "../components/weather.component";
+import GoogleAds from "../components/GoogleAds";
 
 
 
@@ -25,6 +26,10 @@ const Balboa = () => (
     {/* Google Maps Insert */}
     <div className="map">
       <GoogleMaps />
+    </div>
+
+    <div className="ad-class">
+      <GoogleAds slot="4958998199" />
     </div>
 
     {/* Neighborhood Details */}
--- src/pages/coronado.component.js
+++ src/pages/coronado.component.js
@@ -3,7 +3,7 @@ import Sidebar from "../components/sidebar.component";
 import GoogleMaps from "../components/map.component";
 import './secondary.styles.scss';
 import Weather from "../components/weather.component";
-
+import GoogleAds from "../components/GoogleAds";
 
 
 const Coronado = () => (
@@ -24,6 +24,10 @@ const Coronado = () => (
     {/* Google Maps Insert */}
     <div className="map">
       <GoogleMaps />
+    </div>
+    
+    <div className="ad-class">
+      <GoogleAds slot="6874715094" />
     </div>
 
     {/* Neighborhood Details */}
